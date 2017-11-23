9be981934a2718c1cd4a3b5ca1796397fff37bc3
Pawin Thepbanchaporn <w.i.n@me.com>
2017-11-24 01:51:48 +0700

    Remove bot id

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import time
 from slackclient import SlackClient
 import cmc_api as api
 
-# starterbot's ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
-
-# constants
-AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
-
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
